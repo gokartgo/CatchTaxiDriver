b6d7ee3264af34ac87b6119e34bc38c2c674f7e0
gokartgo <gokart.007@hotmail.com>
2017-12-12 23:27:06 +0700

    add sign in page

---
+++ app/src/main/java/s8010027/kritchanon/catchtaxidriver/activity/SignInActivity.java
@@ -0,0 +1,15 @@
+package s8010027.kritchanon.catchtaxidriver.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import s8010027.kritchanon.catchtaxidriver.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
